6fe51e0b412932bf7732d31d5708d06063967d91
Andrew Cholakian <andrew@andrewvc.com>
2010-08-23 15:54:45 -0700

    JobQueue fix

--- cookbooks/jobqueue/recipes/default.rb
+++ cookbooks/jobqueue/recipes/default.rb
@@ -7,18 +7,20 @@ require 'etc'
 
 if ['solo','app_master'].include?(node[:instance_role])
   # Install the JobQueue monit file.
-  template '/etc/monit.d/jobqueue.monitrc' do
-    ey_cloud_report "JobQueue" do
-      message "Installing JobQueue Monit Config"
+  run_for_app('cocodot') do
+    template '/etc/monit.d/jobqueue.monitrc' do
+      ey_cloud_report "JobQueue" do
+        message "Installing JobQueue Monit Config"
+      end
+      owner node[:owner_name]
+      group node[:owner_name]
+      source 'jobqueue.monitrc.erb'
+      variables({
+        :env      => 'staging',
+        :pid_file => '/tmp/job_queue.pid'
+      })
     end
-    owner node[:owner_name]
-    group node[:owner_name]
-    source 'jobqueue.monitrc.erb'
-    variables({
-      :env      => 'staging',
-      :pid_file => '/tmp/job_queue.pid'
-    })
-  end
 
-  execute "monit quit"
+    execute "monit quit"
+  end
 end
