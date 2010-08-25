41fd3ad1bd397fc7968fa0b6859058866a74f96b
Gal Steinitz <gal@steinitz.com>
2010-08-25 10:10:00 -0700

    nginx chef recipe

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -13,5 +13,25 @@ if ['solo','app_master'].include?(node[:instance_role])
   template '/etc/nginx/mime.types' do
     source 'mime_types.erb'
   end
+
+  # execute 'add client_max_body_size to 10mb' do
+    nginx_conf_path = '/etc/nginx/nginx.conf'
+    conf = open(nginx_conf_path).read
+    if !conf.include?('client_max_body_size')
+      directive = "  client_max_body_size 10m;"
+      lines = conf.split("\n")
+      insert_after = lines.index "http {"
+      lines.insert(insert_after+1,directive)
+      File.open(nginx_conf_path, 'w') do |f|  
+        f.puts lines.join("\n")
+      end
+    end
+  # end
+  
+  execute "Restart nginx" do
+    command %Q{
+      /etc/init.d/nginx restart
+    }
+  end
   
 end
