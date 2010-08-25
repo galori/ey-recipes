1b3a93848d44eeddd7c5e8452388a537ded30f9a
Gal Steinitz <gal@steinitz.com>
2010-08-24 18:46:09 -0700

    added mime types template

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -10,21 +10,8 @@ if ['solo','app_master'].include?(node[:instance_role])
       message "Adding mime types to nginx config"
   end
   
-  execute "adding mime types to nginx config" do
-    additional_mime_types = [
-      {:content_type  => 'application/xslt+xml', :extension => 'xsl'},
-      {:content_type  => 'application/maltese',  :extension => 'maltese'}
-    ]
-  
-    mime_types_string = additional_mime_types.map{|m| "    #{m[:content_type]}                  #{m[:extension]};\n"}.join("") 
-
-    mime_types_file_path = "/etc/nginx/mime.types"
-    file = open(mime_types_file_path)
-    mime_types = file.read
-    mime_types.gsub!(/}$/,"#{mime_types_string}}")
-  
-    File.open(mime_types_file_path, 'w') do |f|  
-      f.puts mime_types
-    end
+  template '/etc/nginx/mime.types' do
+    source 'mime_types.erb'
   end
+  
 end
