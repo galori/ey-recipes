2ef9c465aa09c3059d1deb05cceb2a09b1255c47
Andrew Cholakian <andrew@andrewvc.com>
2010-09-17 16:17:55 -0700

    Refs ticket #1697. Tweaks to unicorn tweaks

--- cookbooks/tune_unicorn/recipes/default.rb
+++ cookbooks/tune_unicorn/recipes/default.rb
@@ -9,9 +9,14 @@ if ['solo','app_master','app'].include?(node[:instance_role])
   execute "sh -c 'sed -e \"s/^worker_processes.*$/worker_processes 2/\" /data/publisher/shared/config/unicorn.rb > /data/publisher/shared/config/unicorn_custom.rb'"
   execute "sh -c 'sed -e \"s/^worker_processes.*$/worker_processes 3/\" /data/cocodot/shared/config/unicorn.rb > /data/cocodot/shared/config/unicorn_custom.rb'"
   
-  template '/data/cocodot/shared/config/env.custom' do
-    owner node[:owner_name]
-    group node[:owner_name]
-    source 'env.custom.erb'
+  ['/data/cocodot/shared/config','/data/publisher/shared/config'].each do |env_dir|
+    template "#{env_dir}/env.custom" do
+      owner node[:owner_name]
+      group node[:owner_name]
+      source 'env.custom.erb'
+      variables({
+        :unicorn_config_path => "#{env_dir}/unicorn_custom.rb"
+      })
+    end
   end
 end
