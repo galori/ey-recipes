d2126a6e6f151b2aea14f2181476fd73379beb9f
Andrew Cholakian <andrew@andrewvc.com>
2010-09-22 10:41:49 -0700

    Lower # of unicorns

--- cookbooks/tune_unicorn/recipes/default.rb
+++ cookbooks/tune_unicorn/recipes/default.rb
@@ -6,8 +6,6 @@
 require 'etc'
 
 if ['solo','app_master','app'].include?(node[:instance_role])
-  execute "sudo sh -c 'echo 10 > /proc/sys/vm/swappiness'"
-  
   execute "sh -c 'sed -e \"s/^worker_processes.*$/worker_processes 2/\" /data/publisher/shared/config/unicorn.rb > /data/publisher/shared/config/unicorn_custom.rb'"
   execute "sh -c 'sed -e \"s/^worker_processes.*$/worker_processes 3/\" /data/cocodot/shared/config/unicorn.rb > /data/cocodot/shared/config/unicorn_custom.rb'"
   
