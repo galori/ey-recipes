bf04cc664a67c2763b6db8ddf11b791dd949f271
Andrew Cholakian <andrew@andrewvc.com>
2010-09-22 17:59:40 -0700

    Fixed binaries for small servers

--- cookbooks/xorg/recipes/default.rb
+++ cookbooks/xorg/recipes/default.rb
@@ -5,7 +5,7 @@
 
 require 'etc'
 
-if ['app','app_master','solo'].include?(node[:instance_role])
+if ['app','app_master','solo','util'].include?(node[:instance_role])
   run_for_app('cocodot') do
     ey_cloud_report "xorg" do
       message "Installing X packages for flash"
@@ -13,6 +13,6 @@ if ['app','app_master','solo'].include?(node[:instance_role])
 
     execute "sh -c 'cp -rfv /data/ey-data/binaries/x11-packages/* /engineyard/portage/packages/'"
     execute "emerge -k xorg-server nss nspr gtk+"
-    execute "cp -f /data/binaries/flashplayer /usr/local/bin/"
+    execute "cp -f /data/ey-data/binaries/flashplayer /usr/local/bin/"
   end
 end
