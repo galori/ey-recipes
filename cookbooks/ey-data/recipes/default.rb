6a64cf562de3988b3718e4ca3b98f92d1f292ee7
Andrew Cholakian <andrew@andrewvc.com>
2010-09-27 11:54:02 -0700

    Fixed broken commands in ey-data

--- cookbooks/ey-data/recipes/default.rb
+++ cookbooks/ey-data/recipes/default.rb
@@ -19,17 +19,19 @@ if ['solo','app_master','app','util'].include?(node[:instance_role])
 
   ey_data_dir = '/data/ey-data'
 
+  ey_data_cmd = "sudo -u deploy sh -c 'rm -Rf #{ey_data_dir}; git clone --depth 1 git@cocodot.unfuddle.com:cocodot/ey-data.git #{ey_data_dir} && cd #{ey_data_dir}'"
   #Get the latest copy of the configs
-  execute "Get Latest ey-data" do
-    branch = node['environment']['framework_env']
-    command "sudo -u deploy sh -c 'rm -Rf #{ey_data_dir}; git clone --depth 1 git@cocodot.unfuddle.com:cocodot/ey-data.git #{ey_data_dir} && cd #{ey_data_dir} && git checkout -b #{branch} origin/#{branch}'"
+  execute ey_data_cmd do
+    command ey_data_cmd
   end
   
   execute "Ensure cocodot shared config dir" do
     command "mkdir -p /data/cocodot/shared/config"
   end
-  
-  execute "Symlinking ey-data configs" do
-    command "ln -sf #{ey_data_dir}/shared_config/* /data/cocodot/shared/config/"
+
+  ey_data_env_conf_dir = "#{ey_data_dir}/shared_config/#{node['environment']['name']}"
+  sym_cmd  = "sh -c 'ls -1  #{ey_data_env_conf_dir} | grep \.yml$ | xargs -n1 -IFILE cp -f #{ey_data_env_conf_dir}/FILE /data/cocodot/shared/config/FILE'"
+  execute sym_cmd do
+    command sym_cmd
   end
 end
