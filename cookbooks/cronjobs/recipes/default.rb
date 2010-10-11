b894fdd361689ae867b9236b527ed9b828578646
Andrew Cholakian <andrew@andrewvc.com>
2010-10-11 14:19:12 -0700

    Resolves ticket #1769. Made all cronjobs run nice -n 20

--- cookbooks/cronjobs/recipes/default.rb
+++ cookbooks/cronjobs/recipes/default.rb
@@ -37,28 +37,28 @@ if ['util','solo'].include?(node[:instance_role])
     minute "0"
     hour "*/1"
     user "deploy"
-    command "cd /data/cocodot/current && script/runner lib/cron/update_accessible_lookup.rb > /data/cocodot/shared/cron_logs/update_accessible_lookup.log"
+    command "cd /data/cocodot/current && nice -n 20 script/runner lib/cron/update_accessible_lookup.rb > /data/cocodot/shared/cron_logs/update_accessible_lookup.log"
   end
 
   cron "Category Rebuild" do
     minute "0"
     hour "*/1"
     user "deploy"
-    command "cd /data/cocodot/current && script/runner 'Category.rebuild!' > /data/cocodot/shared/cron_logs/category_rebuild.log"
+    command "cd /data/cocodot/current && nice -n 20 script/runner 'Category.rebuild!' > /data/cocodot/shared/cron_logs/category_rebuild.log"
   end
   
   cron "Purge Stale Products and Address Books" do
     user "deploy"
     minute "0"
     hour "1"
-    command "cd /data/cocodot/current && rake cocodot:purge_stale_products_and_address_books > /data/cocodot/shared/cron_logs/purge_stale_products_and_address_books.log"
+    command "cd /data/cocodot/current && nice -n 20 rake cocodot:purge_stale_products_and_address_books > /data/cocodot/shared/cron_logs/purge_stale_products_and_address_books.log"
   end
 
   if node['environment']['framework_env'] == 'production'
     cron "Mark & Purge Bounces" do
       minute "*/30"
       user "deploy"
-      command "cd /data/cocodot/current && rake cocodot:mark_and_purge_bounces > /data/cocodot/shared/cron_logs/mark_and_purge_bounces.log"
+      command "cd /data/cocodot/current && nice -n 20 rake cocodot:mark_and_purge_bounces > /data/cocodot/shared/cron_logs/mark_and_purge_bounces.log"
     end
      
     cron "Recurring Biller" do
@@ -66,21 +66,21 @@ if ['util','solo'].include?(node[:instance_role])
       hour "1"
       day  "*"
       user "deploy"
-      command "cd /data/cocodot/current && ruby script/runner script/recurring_biller > /data/cocodot/shared/cron_logs/recurring_biller.log"
+      command "cd /data/cocodot/current && nice -n 20 ruby script/runner script/recurring_biller > /data/cocodot/shared/cron_logs/recurring_biller.log"
     end
 
     cron "Daily Mailer" do
       user "deploy"
       minute "0"
       hour "1"
-      command "cd /data/cocodot/current && ruby script/runner script/daily_mailer > /data/cocodot/shared/cron_logs/daily_mailer.log"
+      command "cd /data/cocodot/current && nice -n 20 ruby script/runner script/daily_mailer > /data/cocodot/shared/cron_logs/daily_mailer.log"
     end
 
     cron "Non-Sub Initial Blast" do
       user "deploy"
       minute "0"
       hour "1"
-      command "cd /data/cocodot/current && rake cocodot:non_sub_initial_blast > /data/cocodot/shared/cron_logs/non_sub_initial_blast.log"
+      command "cd /data/cocodot/current && nice -n 20 rake cocodot:non_sub_initial_blast > /data/cocodot/shared/cron_logs/non_sub_initial_blast.log"
     end
   end
 end
